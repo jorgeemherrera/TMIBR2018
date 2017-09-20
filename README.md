791b8b220a31390f143febc8f1f410023fb76a76
Jorge Eliecer <jorge.m.h@outlook.com>
2017-09-20 14:26:16 -0500

    Added Canvas js, & some structure of Header

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # New Landing Page - TecnoMultimedia InfoComm Brasil 2018
+
+- Use a Video Background
+- Just an input to register people
+- Use some parallax effects
+- Try to set up a Canvas Objects
---
+++ js/canvas.js
@@ -0,0 +1,81 @@
+
+var canvas = document.getElementById('canvas'),
+    header = document.querySelector('header'),
+    content = document.getElementById('content'),
+    ctx = canvas.getContext('2d');
+
+function mouseIsOn(event) {
+
+    content.style.transition = 'none';
+    content.style.top = -(event.clientY / 20) + 'px';
+    content.style.left = -(event.clientX / 20) + 'px';
+}
+
+function mouseIsOut(event) {
+
+    content.style.transition = 'all 0.33s ease';
+    content.style.top = '0px';
+    content.style.left = '0px';
+}
+
+
+header.addEventListener('mousemove', mouseIsOn);
+header.addEventListener('mouseout', mouseIsOut);
+
+
+ctx.lineWidth = 3;
+ctx.beginPath();
+ctx.strokeStyle = '#039BE5';
+ctx.moveTo(100, 100);
+ctx.lineTo(125, 100);
+ctx.lineTo(112, 125);
+ctx.closePath();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = '#F57F17';
+ctx.moveTo(250, 150);
+ctx.lineTo(275, 150);
+ctx.lineTo(275, 175);
+ctx.lineTo(250, 175);
+ctx.closePath();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = '#E040FB';
+ctx.moveTo(500, 300);
+ctx.lineTo(525, 300);
+ctx.lineTo(512, 325);
+ctx.closePath();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = '#FF80AB';
+ctx.moveTo(750, 250);
+ctx.lineTo(775, 250);
+ctx.lineTo(775, 275);
+ctx.lineTo(750, 275);
+ctx.closePath();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = '#00BCD4';
+ctx.moveTo(1000, 150);
+ctx.lineTo(1025, 150);
+ctx.lineTo(1012, 175);
+ctx.closePath();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = '#00BCD4';
+ctx.moveTo(500, 250);
+ctx.lineTo(525, 250);
+ctx.closePath();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = '#EEFF41';
+ctx.moveTo(1100, 250);
+ctx.lineTo(1125, 250);
+ctx.closePath();
+ctx.stroke();
