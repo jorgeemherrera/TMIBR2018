147d36dcbd358a06a732c60008587e57a12c5b1e
Jorge Eliecer <jorge.m.h@outlook.com>
2017-10-12 23:14:42 -0500

    smooth jQuery animation implement

--- js/testimonial.js
+++ js/testimonial.js
@@ -1,1 +1,23 @@
-new WOW().init();
+//Smooth Animation | Jorge Eliecer Munoz Herrera
+//Resource: w3Schools
+$(document).ready(function(){
+	//Agregar smooth scrolling a todos los enlaces
+	$('a').on('click', function(evento){
+		//Estar seguro que this.hash tiene un valor antes de anular el comportamiento predeterminado
+		if(this.hash !== ""){
+			//Prevenir comportamiento predeterminado en el clic
+			evento.preventDefault();
+		//Almacenar el hash
+		var hash = this.hash;
+
+		//Usar metodo jQuery animate() para agregar smooth page scroll
+		//el numero opcion es (800) especifica el numero de milisegundos que toma el scroll hasta el area definida en el enlace
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top}, 800, function(){
+
+			//agregar el hash (#) a la url cuando termine el scroll
+			window.location.hash = hash;
+		});
+	}//end if
+	});
+});
